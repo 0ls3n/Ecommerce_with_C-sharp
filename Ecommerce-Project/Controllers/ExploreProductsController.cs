a9e94933fa37c719c3047b5a7839747967e84533
Rasmus Olsen <Rasmus782@gmail.com>
2024-09-15 22:06:27 +0200

    Made a new view for the explore page

---
+++ Ecommerce-Project/Controllers/ExploreProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Ecommerce_Project.Models.Domain_Classes;
+using Ecommerce_Project.Models.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce_Project.Controllers
+{
+    public class ExploreProductsController : Controller
+    {
+
+        private readonly IRepository<Product> _productRepo;
+
+        public ExploreProductsController(IRepository<Product> productRepo)
+        {
+            _productRepo = productRepo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _productRepo.GetAllAsync());
+        }
+    }
+}
